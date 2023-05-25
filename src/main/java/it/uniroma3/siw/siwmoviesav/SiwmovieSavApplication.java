4afc0c67a6e259b80d363572c69d8afa3e7d1831
SaverioPiana <pianasave@gmail.com>
2023-05-25 16:33:42 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/siwmoviesav/SiwmovieSavApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwmoviesav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwmovieSavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwmovieSavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siwmoviesav/SiwmovieSavApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwmoviesav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwmovieSavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

49f7dd774832eefba9842fa72fe240e3eee0fc3c
SaverioPiana <pianasave@gmail.com>
2023-05-25 17:39:54 +0200

    controller e repo for Movie

---
+++ src/main/java/it/uniroma3/siw/siwmoviesav/model/Movie.java
@@ -0,0 +1,65 @@
+package it.uniroma3.siw.siwmoviesav.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private Integer year;
+    private String urlImage;
+    public Movie(){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(title, movie.title) && Objects.equals(year, movie.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, year);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
---
+++ src/main/java/it/uniroma3/siw/siwmoviesav/repository/MovieRepository.java
@@ -0,0 +1,11 @@
+package it.uniroma3.siw.siwmoviesav.repository;
+
+import it.uniroma3.siw.siwmoviesav.model.Movie;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MovieRepository extends CrudRepository<Movie, Long> {
+    public List<Movie> findByYear(Integer year);
+    public boolean existsByTitleAndYear(String Title, Integer year);
+}

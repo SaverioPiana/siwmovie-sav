98913d03136dee50392df5a43331a4920592d439
Saverio <pianasave@gmail.com>
2023-05-30 16:48:58 +0200

    updating movie html stuff

--- src/main/java/it/uniroma3/siw/siwmoviesav/controller/ArtistController.java
+++ src/main/java/it/uniroma3/siw/siwmoviesav/controller/ArtistController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ArtistController {
@@ -15,5 +16,11 @@ public class ArtistController {
     public String showArtists(Model model){
         model.addAttribute("artists", artistRepository.findAll());
         return "artists";
+    }
+
+    @GetMapping("/artist/{id}")
+    public String getArtist(@PathVariable("id") Long id, Model model){
+        model.addAttribute("artist", artistRepository.findById(id).get());
+        return "artist";
     }
 }

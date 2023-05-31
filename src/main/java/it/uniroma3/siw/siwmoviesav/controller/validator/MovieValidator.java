3c84c7bee703c5f978a757874a7d689887fab114
SaverioPiana <pianasave@gmail.com>
2023-05-31 14:33:20 +0200

    custom validator

---
+++ src/main/java/it/uniroma3/siw/siwmoviesav/controller/validator/MovieValidator.java
@@ -0,0 +1,26 @@
+package it.uniroma3.siw.siwmoviesav.controller.validator;
+
+import it.uniroma3.siw.siwmoviesav.model.Movie;
+import it.uniroma3.siw.siwmoviesav.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+@Component
+public class MovieValidator implements Validator {
+    @Autowired
+    private MovieRepository movieRepository;
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Movie.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Movie movie = (Movie)target;
+        if(movie.getTitle()!= null && movie.getYear()!=null
+            && movieRepository.existsByTitleAndYear(movie.getTitle(), movie.getYear())){
+            errors.reject("movie.duplicates");
+        }
+    }
+}
